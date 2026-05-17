d2da3b25661edeae389a8a81b9b0ef2dc59d7079
Adam <adambare165@gmail.com>
2026-05-16 21:21:49 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
-import './index.css'
+//import './index.css'
 import Test from './Test';
 //lifting part
 import Parent from './Parent';
 import ModalCoponant from './ModalCoponant';
-
+import { FaTrashAlt } from 'react-icons/fa';
+import { FaPlus, FaTrash } from 'react-icons/fa6';
+import Groceries from './Groceries';
 //
+import './grocery_app.css'
 
 const itemListStyle = {
   textAlign: "center",
@@ -15,54 +18,17 @@ const itemListStyle = {
 
 //componant
 function App() {
-  const [items, setItems] = useState([]);
-  const [input, setInput] = useState("")
-  function addItem() {
-    if (input.trim() === "") return;
-    const newItem = {
-      id: Date.now(),
-      name: input
-    };
-    // setItems([{ ...items, newItem }])
-    setItems(prev => [...prev, newItem]);
-    setInput("")
-  };
-  function removeItem(id) {
-    setItems(items.filter((item) => item.id !== id));
 
-  }
   return (
     <>
-      <Test />
-      <hr />
-
-      <div className='container'>
-        <h1 style={{ textDecoration: "underline" }}>weolcome to my  Shop</h1>
-        <section className="input-group">
-          <input type="text" placeholder='add item'
-            value={input}
-            onChange={(e) => setInput(e.target.value)} />
-            
-          {/*           <button onClick={addItem}>add item</button> */}
-
-          <button onClick={addItem}>add item</button>
-
-        </section>
 
 
-        {items.map((item) => <li key={item.id}>{item.name}
-          <small onClick={() => removeItem(item.id)}
-            title='remove item'>❌</small>
-        </li>)}
-        <p style={itemListStyle}>{items.length < 1 ? "no items yet" : "items:" + items.length}</p>
-
-
-
-      </div>
       <div>
-        <ModalCoponant />
-        <hr />
-        <Parent />
+
+        <Groceries />
+        <footer>
+          <p>Groceries Tracker . &copy; {new Date().getFullYear()}</p>
+        </footer>
       </div>
     </>
   );
---
+++ src/Groceries.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+//import './index.css'
+import './grocery_app.css'
+import { FaPlus, FaTrash } from 'react-icons/fa6';
+import { useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
+import { FaShoppingBasket, FaShoppingCart, FaShoppingBag } from 'react-icons/fa';
+
+const itemListStyle = {
+    textAlign: "center",
+    marginTop: "2.5rem",
+
+};
+
+function Groceries() {
+    const [items, setItems] = useState([]);
+    const [input, setInput] = useState("")
+    function addItem() {
+        if (input.trim() === "") return;
+        const newItem = {
+            id: Date.now(),
+            name: input,
+            bought: false
+        };
+        // setItems([{ ...items, newItem }])
+        setItems(prev => [...prev, newItem]);
+        setInput("")
+    };
+    function removeItem(id) {
+        setItems(items.filter((item) => item.id !== id));
+
+    }
+    function toggleItem(id) {
+        setItems(prev => prev.map((item) => {
+            if (item.id === id) {
+                return { ...item, bought: !item.bought }
+            }
+            return item;
+        }))
+    };
+    function hundleKeyDown(e) {
+        if (e.key === "Enter") {
+            addItem();
+        }
+    }
+    return (
+        <>
+            <div className='container'>
+                <div className='header'>
+                    <h1>Groceries List</h1>
+
+                    <FaShoppingCart className='cart-icon'/>
+
+                </div>
+
+
+                <section className="input-group">
+                    <input type="text" placeholder='add item'
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                        onKeyDown={hundleKeyDown}
+                        className='grocery-input' />
+
+                    {/*           <button onClick={addItem}>add item</button> */}
+
+                    <FaPlus onClick={addItem} className='add-icon'></FaPlus>
+
+                </section>
+
+
+                <section>
+                    {items.map((item) => <li key={item.id}
+                        className={item.bought ? "bought" : ""}>
+                        <input type="checkbox"
+                            onChange={() => toggleItem(item.id)} />
+                        {item.name}
+                        <small onClick={() => removeItem(item.id)}
+                            title='remove item'><FaTrashAlt className='trash-icon' /></small>
+                    </li>)}
+                    <p style={itemListStyle}>{items.length < 1 ? "no items yet" : "items:" + items.length}</p>
+                </section>
+
+            </div>
+
+        </>
+    )
+
+
+}
+
+export default Groceries
