206f9f087a9d97e094ce3f24e3f57e39ddfc414f
Adam <adambare165@gmail.com>
2026-07-29 19:46:25 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,21 @@
-import { useState } from 'react';
+import { useState } from "react";
 //import './index.css'
-import Test from './Test';
+import Test from "./Test";
 //lifting part
-import Parent from './Parent';
-import ModalCoponant from './ModalCoponant';
-import { FaTrashAlt } from 'react-icons/fa';
-import { FaPlus, FaTrash } from 'react-icons/fa6';
-import Groceries from './Groceries';
+import Parent from "./Parent";
+import ModalCoponant from "./ModalCoponant";
+import { FaTrashAlt } from "react-icons/fa";
+import { FaPlus, FaTrash } from "react-icons/fa6";
+import Groceries from "./Groceries";
 //
-import './grocery_app.css'
-
-
+import "./grocery_app.css";
 
 //componant
 function App() {
-
   return (
     <>
-
-
-      <div className='layout'>
-
+      <div className="layout">
         <Groceries />
-        <footer>
-          <p>Groceries Tracker . &copy; {new Date().getFullYear()}</p>
-        </footer>
       </div>
     </>
   );
--- src/Groceries.jsx
+++ src/Groceries.jsx
@@ -1,92 +1,155 @@
-import React, { useEffect } from 'react'
-//import './index.css'
-import './grocery_app.css'
-import { FaPlus, FaTrash } from 'react-icons/fa6';
-import { useState } from 'react';
-import { FaTrashAlt } from 'react-icons/fa';
-import { FaShoppingBasket, FaShoppingCart, FaShoppingBag } from 'react-icons/fa';
+import React, { useEffect } from "react";
 
-
+import "./grocery_app.css";
+//import "./App.css";
+import { FaPlus, FaTrash } from "react-icons/fa6";
+import { useState } from "react";
+import { FaTrashAlt, FaEdit, FaRegSave, FaTimes } from "react-icons/fa";
+import {
+  FaShoppingBasket,
+  FaShoppingCart,
+  FaShoppingBag,
+} from "react-icons/fa";
 
 function Groceries() {
-    const [items, setItems] = useState(JSON.parse(localStorage.getItem("Item")) || []);
-    const [input, setInput] = useState("")
-    function addItem() {
-        if (input.trim() === "") return;
-        const newItem = {
-            id: Date.now(),
-            name: input,
-            bought: false
-        };
-        // setItems([{ ...items, newItem }])
-        setItems(prev => [...prev, newItem]);
-        setInput("")
-    };
-    function removeItem(id) {
-        setItems(items.filter((item) => item.id !== id));
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("Item")) || [],
+  );
+  const [input, setInput] = useState("");
+  //update states
+  const [editId, seteditId] = useState(null);
+  const [editText, seteditText] = useState("");
+  function editItem(id) {
+    const item = items.find((i) => i.id == id);
+    seteditId(id);
+    seteditText(item.name);
+  }
+  function updateItems() {
+    const newItems = items.map((i) =>
+      i.id === editId ? { ...i, name: editText } : i,
+    );
+    setItems(newItems);
+    seteditId(null);
+  }
 
-    }
-    function toggleItem(id) {
-        setItems(prev => prev.map((item) => {
-            if (item.id === id) {
-                return { ...item, bought: !item.bought }
-            }
-            return item;
-        }))
+  function addItem() {
+    if (input.trim() === "") return;
+    const newItem = {
+      id: Date.now(),
+      name: input,
+      bought: false,
     };
-    useEffect(() => {
-        localStorage.setItem("Item", JSON.stringify(items));
-    }, [items])
-    function hundleKeyDown(e) {
-        if (e.key === "Enter") {
-            addItem();
+    // setItems([{ ...items, newItem }])
+    setItems((prev) => [...prev, newItem]);
+    setInput("");
+  }
+  function removeItem(id) {
+    setItems(items.filter((item) => item.id !== id));
+  }
+  function toggleItem(id) {
+    setItems((prev) =>
+      prev.map((item) => {
+        if (item.id === id) {
+          return { ...item, bought: !item.bought };
         }
+        return item;
+      }),
+    );
+  }
+  useEffect(() => {
+    localStorage.setItem("Item", JSON.stringify(items));
+  }, [items]);
+  function hundleKeyDown(e) {
+    if (e.key === "Enter") {
+      addItem();
     }
-    return (
-        <>
-            <div className='container'>
-                <div className='header'>
-                    <h1>Groceries List</h1>
+  }
+  return (
+    <>
+      <div className="grocery-app-con">
+        <section className="header">
+          <header className="groc-header">
+            <h1>Groceries List</h1>
+            <FaShoppingCart className="cart-icon" />
+            <p> Everything you need all in one place</p>
+          </header>
+        </section>
 
-                    <FaShoppingCart className='cart-icon' />
+        <section className="input-group">
+          <input
+            type="text"
+            placeholder="add item"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onKeyDown={hundleKeyDown}
+            className="grocery-input"
+          />
+          <FaPlus onClick={addItem} className="add-icon"></FaPlus>
+        </section>
 
-                </div>
+        <section className="grocery-list-cont">
+          <ul>
+            {items.map((item) => (
+              <li key={item.id} className={item.bought ? "bought" : ""}>
+                {editId === item.id ? (
+                  <section className="edit-sect">
+                    <input
+                      type="text"
+                      value={editText}
+                      onChange={(e) => seteditText(e.target.value)}
+                      className="edit-input"
+                    />
+                    <FaRegSave onClick={updateItems} className="save-icon">
+                      Save
+                    </FaRegSave>
+                    <FaTimes
+                      className="cancel-icon"
+                      onClick={() => seteditId(null)}
+                    >
+                      cancel
+                    </FaTimes>
+                  </section>
+                ) : (
+                  <div className="items-list-card">
+                    <p> {item.name}</p>
+                    <FaEdit onClick={() => editItem(item.id)}>edit</FaEdit>
+                    <input
+                      type="checkbox"
+                      onChange={() => toggleItem(item.id)}
+                    />
 
+                    <small
+                      onClick={() => removeItem(item.id)}
+                      title="remove item"
+                    >
+                      <FaTrashAlt className="trash-icon" />
+                    </small>
+                  </div>
+                )}
+              </li>
+            ))}
+          </ul>
 
-                <section className="input-group">
-                    <input type="text" placeholder='add item'
-                        value={input}
-                        onChange={(e) => setInput(e.target.value)}
-                        onKeyDown={hundleKeyDown}
-                        className='grocery-input' />
+          {/* <p className='items-count'>{items.length < 1 ? " No items yet" : "you have: " + items.length === 1 ? " item" : + " items"}</p> */}
+          <p className="items-count">
+            {items.length === 0
+              ? "Add your items"
+              : items.length === 1
+                ? "you have 1 item"
+                : "you have " + items.length + " items"}
+          </p>
 
-                    {/*           <button onClick={addItem}>add item</button> */}
-
-                    <FaPlus onClick={addItem} className='add-icon'></FaPlus>
-
-                </section>
-
-
-                <section>
-                    {items.map((item) => <li key={item.id}
-                        className={item.bought ? "bought" : ""}>
-                        <input type="checkbox"
-                            onChange={() => toggleItem(item.id)} />
-                        {item.name}
-                        <small onClick={() => removeItem(item.id)}
-                            title='remove item'><FaTrashAlt className='trash-icon' /></small>
-                    </li>)}
-                    {/* <p className='items-count'>{items.length < 1 ? " No items yet" : "you have: " + items.length === 1 ? " item" : + " items"}</p> */}
-                    <p className='items-count'>{items.length === 0 ? "No items yet" : items.length === 1 ? "you have 1 item"
-                        : "you have " + items.length + " items"}</p>
-                </section>
-
-            </div>
-
-        </>
-    )
-
-
+          <button
+            className="clear-all-btn"
+            onClick={() => setItems([])}
+            title="are you sure you want to clear all your tasks❗"
+          >
+            Clear all task
+          </button>
+        </section>
+      </div>
+    </>
+  );
 }
 
-export default Groceries
+export default Groceries;
